da2b0344d655dab845e0c2b4e35a11823b8fe0de
Anel Danza <aneldanza23@gmail.com>
2024-08-05 11:46:52 -0400

    update baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://127.0.0.1:3001/",
+    baseUrl: "http://127.0.0.1:3001",
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
 
