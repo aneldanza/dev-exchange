18e1661134c13d0d6434d11b1f14b545e58e5363
Anel Danza <aneldanza23@gmail.com>
2024-08-05 12:01:33 -0400

    add Outlet and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { SignUpForm } from "./components/auth/SignupForm";
+import { Outlet } from "react-router-dom";
+import { Header } from "./components/layout/Header";
 
-function App() {
+export default function App() {
   return (
     <>
-      <div>
-        <SignUpForm />
+      <div className="font-sans ">
+        <nav className="px-4">
+          <Header />
+        </nav>
+        <main className="py-6 px-4">
+          <Outlet />
+        </main>
       </div>
     </>
   );
 }
-
-export default App;
