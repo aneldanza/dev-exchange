581cfd9eb0f882db47e16e8561a86dc3f6f632ab
Anel Danza <aneldanza23@gmail.com>
2024-08-05 11:23:30 -0400

    Add Sign up form

---
+++ src/components/auth/SignupForm.tsx
@@ -0,0 +1,66 @@
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { useSignUpMutation } from "../../services/api";
+import { InputField } from "../common/InputField";
+
+interface SignUpCredentials {
+  username: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const initialValues: SignUpCredentials = {
+  username: "",
+  email: "",
+  password_confirmation: "",
+  password: "",
+};
+
+const validationSchema = Yup.object({
+  username: Yup.string().required("Username is required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+  password_confirmation: Yup.string()
+    .oneOf([Yup.ref("password"), ""], "Passwords must match")
+    .required("Confirm Password is required"),
+});
+
+export const SignUpForm = () => {
+  const [signUp] = useSignUpMutation();
+
+  const handleSignUp = async (credentials: SignUpCredentials) => {
+    try {
+      const result = await signUp({
+        credentials: { user: credentials },
+      }).unwrap();
+      console.log(result);
+    } catch (e) {}
+  };
+
+  return (
+    <div className="p-8">
+      <div className="font-bold text-xl text-center mb-6">Join DevExchange</div>
+
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSignUp}
+        validationSchema={validationSchema}
+      >
+        <Form className="space-y-6">
+          <InputField name="username" label="Username" />
+          <InputField name="email" label="email" />
+          <InputField name="password" label="password" />
+          <InputField name="password_confirmation" label="confirm password" />
+          <button type="submit" className="btn-primary">
+            Sign up
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
