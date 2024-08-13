2c0db715449c0c07ea99ae93fdb3c44fdf23283e
Anel Danza <aneldanza23@gmail.com>
2024-08-13 11:20:42 -0400

    fetch current user from server

--- src/AuthContext.tsx
+++ src/AuthContext.tsx
@@ -1,4 +1,12 @@
-import { createContext, useContext, useState, ReactNode, type FC } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  type FC,
+  useEffect,
+} from "react";
+import { useGetCurrentUserQuery } from "./services/api";
 
 interface UserInfoLimited {
   username: string;
@@ -21,6 +29,20 @@ const AuthContext = createContext<AuthContextProps>({
 });
 export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
   const [user, setUser] = useState<UserInfoLimited | null>(null);
+  const { data, isError, isLoading, isSuccess, error } =
+    useGetCurrentUserQuery("");
+
+  useEffect(() => {
+    console.log("fetching current user from the server! ");
+    if (isSuccess) {
+      console.log(`got user info: ${data}`);
+      setUser(data.user);
+    } else if (isError) {
+      console.log(error);
+    } else if (isLoading) {
+      console.log("loading user info from the server ...");
+    }
+  }, [isSuccess, isError, isLoading, data, error]);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
