cc3251ef115c866d533641115d8c3329993ebb5f
Anel Danza <aneldanza23@gmail.com>
2024-09-13 14:48:28 -0400

    add path /tags

--- src/services/BrowserRouter.tsx
+++ src/services/BrowserRouter.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { SignUpForm } from "../components/auth/SignupForm";
 import { SignInForm } from "../components/auth/SigninForm";
+import { TagsPage } from "../components/tags/TagsPage";
 
 export const browserRouter = createBrowserRouter([
   {
@@ -10,6 +11,7 @@ export const browserRouter = createBrowserRouter([
     children: [
       { path: "/signup", element: <SignUpForm /> },
       { path: "/login", element: <SignInForm /> },
+      { path: "/tags", element: <TagsPage /> },
     ],
   },
 ]);
