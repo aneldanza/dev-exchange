9b833bb9163b03fe98607e393e4edd13599a4daa
Anel Danza <aneldanza23@gmail.com>
2024-09-13 16:43:35 -0400

    add styling

--- src/components/tags/TagsList.tsx
+++ src/components/tags/TagsList.tsx
@@ -11,9 +11,12 @@ export const TagsList: React.FC<TagsListProps> = ({ tags }) => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
       {tags.map((tag) => (
         <div key={tag.id} className="flex flex-col border rounded-md p-3">
-          <Link to={`/tags/${tag.name}`} className="tag">
-            {tag.name}
-          </Link>
+          <div>
+            <Link to={`/tags/${tag.name}`} className="tag">
+              {tag.name.toLowerCase()}
+            </Link>
+          </div>
+          <div className="text-xs">{tag.description}</div>
         </div>
       ))}
     </div>
