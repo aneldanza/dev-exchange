a68ea2e9a26e1e39f69effa29dbe74fac546027f
Anel Danza <aneldanza23@gmail.com>
2024-09-13 16:06:45 -0400

    Add styling and tags page description

--- src/components/tags/TagsPage.tsx
+++ src/components/tags/TagsPage.tsx
@@ -22,8 +22,17 @@ export const TagsPage: React.FC = () => {
     return <div>Error</div>;
   } else if (isSuccess && tags) {
     return (
-      <div>
-        <h1>Tags</h1>
+      <div className="flex flex-col space-y-4 ">
+        <h1 className="text-2xl">Tags</h1>
+        <div className="text-sm">
+          Tags are keywords or labels associated with questions that allow users
+          to easily filter and search for content relevant to their interests or
+          areas of expertise. This page showcases a list of all available tags,
+          with each tag linking to questions that have been categorized under
+          it. Users can explore various tags to find topics they are
+          knowledgeable about or wish to learn more about, contributing to
+          discussions and answering related questions.
+        </div>
         <ul>
           {tags.map((tag) => (
             <li key={tag.id}>
