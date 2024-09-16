3df45701c6875d32e77db080ceb32473043a1345
Anel Danza <aneldanza23@gmail.com>
2024-09-16 15:09:24 -0400

    add moment library to display time. Update UserProfile

--- src/components/users/UserProfile.tsx
+++ src/components/users/UserProfile.tsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import Button from "../common/Button";
 import { FullUserData } from "./types";
 import { PencilIcon } from "@heroicons/react/20/solid";
@@ -6,7 +7,15 @@ interface UserProfileProps {
   data: FullUserData;
 }
 
+import React, { useState } from "react";
+
 export const UserProfile: React.FC<UserProfileProps> = ({ data }) => {
+  const [activeTab, setActiveTab] = useState("Profile");
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div className="flex flex-col space-y-4">
       <div className="grid">
@@ -17,14 +26,35 @@ export const UserProfile: React.FC<UserProfileProps> = ({ data }) => {
             className="btn-outline"
             icon={<PencilIcon className="w-4" />}
           />
-          {/* <button className="btn-outline justify-end">Edit Profile</button> */}
         </div>
         <div>
           <div className="text-2xl font-semibold">{data.username}</div>
+          <div className="text-xs">{`Member for ${moment(
+            data.createdAt
+          ).fromNow()}`}</div>
         </div>
       </div>
+      <div className="flex gap-2">
+        <div
+          className={`cursor-pointer ${
+            activeTab === "Profile" ? "font-semibold" : ""
+          }`}
+          onClick={() => handleTabClick("Profile")}
+        >
+          Profile
+        </div>
+        <div
+          className={`cursor-pointer ${
+            activeTab === "Activity" ? "font-semibold" : ""
+          }`}
+          onClick={() => handleTabClick("Activity")}
+        >
+          Activity
+        </div>
+      </div>
+      {activeTab === "Profile" && <div>Profile content</div>}
+      {activeTab === "Activity" && <div>Activity content</div>}
       <p>{JSON.stringify(data)}</p>
-      {/* Render the user profile information here */}
     </div>
   );
 };
