5cfe05e78354216a5d78afc3543a7096fe638dd5
Anel Danza <aneldanza23@gmail.com>
2024-09-16 15:57:42 -0400

    Add appGray spectrum of colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,22 @@ export default {
       colors: {
         blue: "#1b75d0",
         red: "#9c2121",
-        appGray: "#0c0d0e",
+        appGray: {
+          50: "#dfe2e4",
+          100: "#9ca4aa",
+          200: "#7b858f",
+          300: "#5a666f",
+          400: "#39464f",
+          500: "#0c0d0e",
+          600: "#0a0b0c",
+          700: "#08090a",
+          800: "#060708",
+          900: "#040506",
+        },
+        appBlack: "#0c0d0e",
+        appOrange: "#ff7e0e",
+        appOrangeDark: "#9b4216",
+
         buttonGray: "#3b4045",
       },
     },
