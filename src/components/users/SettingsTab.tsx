8bab0c51e4de05a48863a3252d8e4464817d90ae
Anel Danza <aneldanza23@gmail.com>
2024-09-30 15:35:31 -0400

    use EditSettings component

--- src/components/users/SettingsTab.tsx
+++ src/components/users/SettingsTab.tsx
@@ -2,31 +2,17 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FullUserData } from "./types";
 import { Dropdown } from "../common/Dropdown";
-import { Formik, Form } from "formik";
-
-import * as Yup from "yup";
-import { RichTextEditor } from "../common/RichTextField";
 import { Button } from "../common/Button";
 import { useDeleteAccountMutation } from "../../services/api";
 import { useAuth } from "../../services/storeHooks";
+import EditSettings from "./EditSettings";
 
 interface SettingsTabProps {
   data: FullUserData;
 }
 
-type FormValues = {
-  about: string;
-};
-
-const validationsSchema = Yup.object().shape({
-  about: Yup.string(),
-});
-
 export const SettingsTab: React.FC<SettingsTabProps> = ({ data }) => {
   const options = ["Edit", "Delete"];
-  const initialValues: FormValues = {
-    about: "",
-  };
 
   const [deleteAccount] = useDeleteAccountMutation();
   const [selectedOption, setSelectedOption] = useState<string>(options[0]);
@@ -78,43 +64,10 @@ export const SettingsTab: React.FC<SettingsTabProps> = ({ data }) => {
             {`${selectedOption} Profile`}
           </div>
           {selectedOption === "Edit" && (
-            <div className="list">
-              <div className="card">
-                <Formik
-                  initialValues={initialValues}
-                  validationSchema={validationsSchema}
-                  onSubmit={(values) => {
-                    console.log(values);
-                  }}
-                >
-                  {() => (
-                    <Form>
-                      <div>
-                        <div>
-                          <RichTextEditor
-                            name="about"
-                            placeholder="Tell us about yourself"
-                            label="About me"
-                          />
-                        </div>
-                      </div>
-                      <div className="flex flex-col gap-4">
-                        <button type="submit" className="btn-primary">
-                          Save
-                        </button>
-
-                        <button
-                          type="reset"
-                          className="btn-outline border-0 text-blue-500 py-2 hover:bg-blue-50"
-                        >
-                          Cancel
-                        </button>
-                      </div>
-                    </Form>
-                  )}
-                </Formik>
-              </div>
-            </div>
+            <EditSettings
+              description={data.description || ""}
+              userId={data.id}
+            />
           )}
           {selectedOption === "Delete" && (
             <div className="w-full">
