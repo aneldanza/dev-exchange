be51af7ca7c599b17511f205045a5ca59d757e9d
Anel Danza <aneldanza23@gmail.com>
2024-10-04 16:26:38 -0400

    add AskQuestion button

--- src/components/questions/AllQuestionsPage.tsx
+++ src/components/questions/AllQuestionsPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useGetAllQuestionsQuery } from "../../services/api";
 import { withLoading } from "../hoc/withLoading";
 import { withError } from "../hoc/withError";
 import { CustomError } from "../common/CustomError";
 import { CustomLoading } from "../common/CustomLoading";
 import QuestionsList from "./QuestionsList";
+import Button from "../common/Button";
 
 const QuestionsListWithErrorAndLoading = withLoading(
   withError(QuestionsList, CustomError),
@@ -12,10 +14,18 @@ const QuestionsListWithErrorAndLoading = withLoading(
 );
 
 const AllQuestionsPage: React.FC = () => {
+  const navigate = useNavigate();
   const { data, error, isLoading } = useGetAllQuestionsQuery(undefined);
   return (
     <div>
-      <h1>All Questions</h1>
+      <div className="flex justify-between">
+        <h1 className="text-xl">All Questions</h1>
+        <Button
+          title="Ask a Question"
+          onClick={() => navigate("/questions/new")}
+          className="btn btn-primary"
+        />
+      </div>
       <QuestionsListWithErrorAndLoading
         questions={data}
         error={error}
