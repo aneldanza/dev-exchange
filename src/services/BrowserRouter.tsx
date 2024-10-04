3864a51819c6fc722a6865b8b2f9fe4fac7432fa
Anel Danza <aneldanza23@gmail.com>
2024-10-04 15:24:24 -0400

    add path for questions

--- src/services/BrowserRouter.tsx
+++ src/services/BrowserRouter.tsx
@@ -5,17 +5,20 @@ import { SignInForm } from "../components/auth/SigninForm";
 import { TagsPage } from "../components/tags/TagsPage";
 import { UserPage } from "../components/users/UserPage";
 import { AllUsersPage } from "../components/users/AllUsersPage";
+import AllQuestionsPage from "../components/questions/AllQuestionsPage";
 
 export const browserRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      { path: "/", element: <AllQuestionsPage /> },
       { path: "/signup", element: <SignUpForm /> },
       { path: "/login", element: <SignInForm /> },
+      { path: "users", element: <AllUsersPage /> },
       { path: "users/:userId", element: <UserPage /> },
       { path: "/tags", element: <TagsPage /> },
-      { path: "users", element: <AllUsersPage /> },
+      { path: "/questions", element: <AllQuestionsPage /> },
     ],
   },
 ]);
