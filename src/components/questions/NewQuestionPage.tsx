4d261448323387b4f2efe1ee3ccff5c9506cd31d
Anel Danza <aneldanza23@gmail.com>
2024-10-07 10:35:43 -0400

    Install react-select and use it for Tags field in NewQuestion form

--- src/components/questions/NewQuestionPage.tsx
+++ src/components/questions/NewQuestionPage.tsx
@@ -1,24 +1,30 @@
 import React from "react";
-import { Formik, Form } from "formik";
+import { Formik, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import Select from "react-select";
 import { InputField } from "../common/InputField";
 import { RichTextEditor } from "../common/RichTextField";
 import Button from "../common/Button";
+import { useGetTagsQuery } from "../../services/api";
+
+const validationSchema = Yup.object({
+  title: Yup.string()
+    .max(50, "Title must be 50 characters or less")
+    .required("Title is required"),
+  description: Yup.string()
+    .max(500, "Description must be 500 characters or less")
+    .required("Description is required"),
+  tags: Yup.array().min(1, "Select at least one tag"),
+});
 
 const NewQuestionPage: React.FC = () => {
   const initialValues = {
     title: "",
     description: "",
+    tags: [],
   };
 
-  const validationSchema = Yup.object({
-    title: Yup.string()
-      .max(50, "Title must be 50 characters or less")
-      .required("Title is required"),
-    description: Yup.string()
-      .max(500, "Description must be 500 characters or less")
-      .required("Description is required"),
-  });
+  const { data: tags } = useGetTagsQuery(undefined);
 
   const onSubmit = (values: typeof initialValues) => {
     console.log("Form data", values);
@@ -33,20 +39,41 @@ const NewQuestionPage: React.FC = () => {
         validationSchema={validationSchema}
         onSubmit={onSubmit}
       >
-        {() => (
+        {(props) => (
           <Form className="space-y-6">
+            <InputField name="title" label="Title" />
+
+            <RichTextEditor
+              name="description"
+              label="Description"
+              placeholder="Add content of your question here..."
+            />
+
             <div>
-              <InputField name="title" label="Title" />
-            </div>
-            <div>
-              <RichTextEditor
-                name="description"
-                label="Description"
-                placeholder="Add content of your question here..."
+              <div className="field-label">Tags</div>
+              <Select
+                options={tags?.map((tag) => ({
+                  value: tag.id,
+                  label: tag.name,
+                }))}
+                onChange={(options) =>
+                  props.setFieldValue(
+                    "tags",
+                    options.map((option) => option.value)
+                  )
+                }
+                isMulti
+              />
+              <ErrorMessage
+                name="tags"
+                component="div"
+                className="error-text"
               />
             </div>
+
             <div className="flex flex-col space-y-6 ">
               <Button
+                type="submit"
                 className="btn btn-primary"
                 title="Post your question"
                 onClick={() => {}}
@@ -56,6 +83,7 @@ const NewQuestionPage: React.FC = () => {
                 className="btn btn-warning"
                 title="Cancel"
                 onClick={() => {}}
+                type="reset"
               />
             </div>
           </Form>
