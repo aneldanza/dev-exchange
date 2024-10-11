fe75743734c9ed4cace62d27ca2db496ea13d238
Anel Danza <aneldanza23@gmail.com>
2024-10-11 13:39:20 -0400

    display question items

--- src/components/questions/QuestionsList.tsx
+++ src/components/questions/QuestionsList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Question } from "./types";
 
 interface QuestionsListProps {
@@ -8,9 +9,31 @@ interface QuestionsListProps {
 const QuestionsList: React.FC<QuestionsListProps> = ({ questions }) => {
   return (
     <div>
-      <ul>
+      <ul className="list divide-y">
         {questions.map((question) => (
-          <li key={question.id}>{question.title}</li>
+          <li key={question.id} className="flex flex-col gap-2">
+            <Link
+              to={`questions/${question.id}`}
+              className="text-blue-500 text-sm"
+            >
+              {question.title}
+            </Link>
+            <div className="text-xs">
+              {question.body.length > 100
+                ? `${question.body.substring(0, 100)}...`
+                : question.body}
+            </div>
+            <div className="flex gap-1">
+              {question.tags.map((tag) => (
+                <span
+                  key={tag.id}
+                  className="bg-gray-200 text-xs px-2 py-1 rounded"
+                >
+                  {tag.name}
+                </span>
+              ))}
+            </div>
+          </li>
         ))}
       </ul>
     </div>
