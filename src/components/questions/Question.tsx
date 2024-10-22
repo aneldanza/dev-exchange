a6de716f3696dd4560a2ef5416101084b0447f69
Anel Danza <aneldanza23@gmail.com>
2024-10-22 14:20:39 -0400

    handle empty question data

--- src/components/questions/Question.tsx
+++ src/components/questions/Question.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import { QuestionData } from "./types";
 
 interface QuestionProps {
-  question: QuestionData;
+  question?: QuestionData;
 }
 
 export const Question: React.FC<QuestionProps> = ({ question }) => {
+  if (!question) {
+    return <div>No question data available.</div>;
+  }
+
   return (
     <div className="question">
-      <h2>{question.title}</h2>
-      <p>{question.body}</p>
+      <h2 className="question-title">{question.title}</h2>
+      <div className="prose">
+        <div dangerouslySetInnerHTML={{ __html: question.body }} />
+      </div>
       <div className="tags">
         {question.tags.map((tag, index) => (
           <span key={index} className="tag">
--- src/components/questions/QuestionPage.tsx
+++ src/components/questions/QuestionPage.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useGetQuestionByIdQuery } from "../../services/api";
-import { type QuestionData } from "./types";
 import { Question } from "./Question";
 import { CustomError } from "../common/CustomError";
 import { CustomLoading } from "../common/CustomLoading";
@@ -9,22 +8,18 @@ import withError from "../hoc/withError";
 import withLoading from "../hoc/withLoading";
 
 const QuestionWithLoadingAndError = withLoading(
-  withError<{ question: QuestionData }>(Question, CustomError),
+  withError(Question, CustomError),
   CustomLoading
 );
 
 const QuestionPage: React.FC = () => {
   const { questionId } = useParams<{ questionId: string }>();
-  const { data, error, isLoading } = useGetQuestionByIdQuery(
-    Number(questionId),
-    {
-      refetchOnMountOrArgChange: true,
-    }
-  );
+  const { data, error, isLoading } = useGetQuestionByIdQuery(questionId || "", {
+    refetchOnMountOrArgChange: true,
+  });
 
   return (
     <div>
-      <h1>Question Page</h1>
       <QuestionWithLoadingAndError
         question={data}
         error={error}
