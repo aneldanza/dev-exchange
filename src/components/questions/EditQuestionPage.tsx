966ae5bd37c6c02de0323a4efa55cab68747b99f
Anel Danza <aneldanza23@gmail.com>
2024-10-23 14:05:31 -0400

    Create EditQuestionPage component

---
+++ src/components/questions/EditQuestionPage.tsx
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import { useUpdateQuestionMutation } from "../../services/api";
+import { useAuth } from "../../services/storeHooks";
+import { QuestionForm } from "./QuestionForm";
+import { FormValues } from "./types";
+
+const EditQuestionPage: React.FC = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const [updateQuestion] = useUpdateQuestionMutation();
+
+  const handleCreateQuestion = async (data: FormValues) => {
+    const formattedTags = data.tags.map((option) => ({
+      name: option.label,
+      id: option.value,
+    }));
+    const newQuestion = await updateQuestion({
+      user_id: user ? user.id : 0,
+      title: data.title,
+      body: data.body,
+      tags: formattedTags,
+    }).unwrap();
+    console.log(newQuestion);
+
+    navigate(`/questions/${newQuestion.id}`);
+  };
+
+  return (
+    <div>
+      <h1 className="font-bold text-xl mb-4 flex">Edit Question</h1>
+      <QuestionForm questionAction={handleCreateQuestion} />
+    </div>
+  );
+};
+
+export default EditQuestionPage;
