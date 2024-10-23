4624a057e1f7fd87bb02799bbf05d2fe6c13bc21
Anel Danza <aneldanza23@gmail.com>
2024-10-23 14:45:21 -0400

    load question form with error and loading

--- src/components/questions/EditQuestionPage.tsx
+++ src/components/questions/EditQuestionPage.tsx
@@ -1,35 +1,61 @@
-import { useNavigate } from "react-router-dom";
-import { useUpdateQuestionMutation } from "../../services/api";
-import { useAuth } from "../../services/storeHooks";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  useUpdateQuestionMutation,
+  useGetQuestionByIdQuery,
+} from "../../services/api";
+// import { useAuth } from "../../services/storeHooks";
 import { QuestionForm } from "./QuestionForm";
 import { FormValues } from "./types";
+import withError from "../hoc/withError";
+import withLoading from "../hoc/withLoading";
+import { CustomError } from "../common/CustomError";
+import { CustomLoading } from "../common/CustomLoading";
+
+const QuestionFormWithErrorAndLoading = withLoading(
+  withError(QuestionForm, CustomError),
+  CustomLoading
+);
 
 const EditQuestionPage: React.FC = () => {
-  const { user } = useAuth();
+  //   const { user } = useAuth();
   const navigate = useNavigate();
+  const { questionId } = useParams<{ questionId: string }>();
+
+  const { data, error, isLoading } = useGetQuestionByIdQuery(questionId || "", {
+    refetchOnMountOrArgChange: true,
+  });
 
   const [updateQuestion] = useUpdateQuestionMutation();
 
-  const handleCreateQuestion = async (data: FormValues) => {
+  const handleUpdateQuestion = async (data: FormValues) => {
     const formattedTags = data.tags.map((option) => ({
       name: option.label,
       id: option.value,
     }));
-    const newQuestion = await updateQuestion({
-      user_id: user ? user.id : 0,
+    const updatedQuestion = await updateQuestion({
       title: data.title,
       body: data.body,
       tags: formattedTags,
     }).unwrap();
-    console.log(newQuestion);
+    console.log(updatedQuestion);
 
-    navigate(`/questions/${newQuestion.id}`);
+    navigate(`/questions/${updatedQuestion.id}`);
   };
 
   return (
     <div>
       <h1 className="font-bold text-xl mb-4 flex">Edit Question</h1>
-      <QuestionForm questionAction={handleCreateQuestion} />
+      <QuestionFormWithErrorAndLoading
+        questionData={{
+          title: data?.title || "",
+          body: data?.body || "",
+          tags: data?.tags || [],
+        }}
+        error={error}
+        isLoading={isLoading}
+        questionAction={handleUpdateQuestion}
+        submitText="Update question"
+      />
     </div>
   );
 };
