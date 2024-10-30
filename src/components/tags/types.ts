eaee2757175a22d99e833c7d1d44929c5d197e08
Anel Danza <aneldanza23@gmail.com>
2024-10-30 17:26:12 -0400

    Add questions array to Tag type

--- src/components/tags/types.ts
+++ src/components/tags/types.ts
@@ -1,6 +1,9 @@
+import { QuestionData } from "../questions/types";
+
 export type Tag = {
   id: number;
   name: string;
   description: string | null;
   createdAt: string;
+  questions: QuestionData[];
 };
