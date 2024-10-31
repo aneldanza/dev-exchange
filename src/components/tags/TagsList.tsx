d0a39dd1ff46b51d592be3a24a9a62223efbc393
Anel Danza <aneldanza23@gmail.com>
2024-10-31 13:52:41 -0400

    add number of questions and created date info to each tag

--- src/components/tags/TagsList.tsx
+++ src/components/tags/TagsList.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import moment from "moment";
 import { Tag } from "./Tag";
 import { TagData } from "./types";
+import { formatCountString } from "../../services/utils";
 
 interface TagsListProps {
   tags: TagData[];
@@ -10,11 +12,17 @@ export const TagsList: React.FC<TagsListProps> = ({ tags }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
       {tags.map((tag) => (
-        <div key={tag.id} className="flex flex-col border rounded-md p-3">
+        <div key={tag.id} className="flex flex-col border rounded-md p-3 gap-3">
           <div>
             <Tag tag={tag} key={tag.id} />
           </div>
-          <div className="text-xs">{tag.description}</div>
+          {tag.description && <div className="text-xs">{tag.description}</div>}
+          <div className="text-xs text-appGray-200 flex justify-between">
+            <div className="">
+              {formatCountString(tag.questions.length, "question", "questions")}
+            </div>
+            <div>{`Created ${moment(tag.created_at).fromNow()}`}</div>
+          </div>
         </div>
       ))}
     </div>
