93550816a860fdd9e5868c1bc03fa851e8329f02
Anel Danza <aneldanza23@gmail.com>
2024-11-04 15:43:42 -0500

    create component to display list of answers

---
+++ src/components/answers/AnswersList.tsx
@@ -0,0 +1,17 @@
+import { type FC } from "react";
+import { AnswerData } from "./types";
+import { Answer } from "./Answer";
+import { formatCountString } from "../../services/utils";
+
+export const AnswersList: FC<{ answers: AnswerData[] }> = ({ answers }) => {
+  return (
+    <div className="flex flex-col">
+      <div>{formatCountString(answers.length, "answer", "answers")}</div>
+      <div className="flex flex-col gap-4">
+        {answers.map((answer) => (
+          <Answer key={answer.id} answer={answer} />
+        ))}
+      </div>
+    </div>
+  );
+};
