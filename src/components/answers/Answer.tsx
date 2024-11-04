043ba4da0f784dbefd626c3967c01a9c37db5ac1
Anel Danza <aneldanza23@gmail.com>
2024-11-04 15:43:14 -0500

    create Answer component

---
+++ src/components/answers/Answer.tsx
@@ -0,0 +1,19 @@
+import { PostMeta } from "../common/PostMeta";
+import { AnswerData } from "./types";
+import { type FC } from "react";
+
+export const Answer: FC<{ answer: AnswerData }> = ({ answer }) => {
+  return (
+    <div className="answer">
+      <div className="answer-body">{answer.body}</div>
+      <div className="flex justify-end">
+        <PostMeta
+          userId={answer.user.id}
+          username={answer.user.username}
+          createdAt={answer.created_at}
+          actionWord="answered"
+        />
+      </div>
+    </div>
+  );
+};
