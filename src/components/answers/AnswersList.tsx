355b5623ed76719b7b098e1413c2b438d46ba457
Anel Danza <aneldanza23@gmail.com>
2024-11-05 16:34:20 -0500

    style Answer container

--- src/components/answers/AnswersList.tsx
+++ src/components/answers/AnswersList.tsx
@@ -5,8 +5,10 @@ import { formatCountString } from "../../services/utils";
 
 export const AnswersList: FC<{ answers: AnswerData[] }> = ({ answers }) => {
   return (
-    <div className="flex flex-col">
-      <div>{formatCountString(answers.length, "answer", "answers")}</div>
+    <div className="flex flex-col my-6">
+      <div className="text-lg">
+        {formatCountString(answers.length, "answer", "answers")}
+      </div>
       <div className="flex flex-col gap-4">
         {answers.map((answer) => (
           <Answer key={answer.id} answer={answer} />
