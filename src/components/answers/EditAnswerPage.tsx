5ac8c3ddb9dfba2420e8cc03a6fe95929bbd911b
Anel Danza <aneldanza23@gmail.com>
2024-11-05 19:41:34 -0500

    Load EditAnswer component with answer data

--- src/components/answers/EditAnswerPage.tsx
+++ src/components/answers/EditAnswerPage.tsx
@@ -1,13 +1,29 @@
 import { useParams } from "react-router-dom";
+import { useGetAnswerByIdQuery } from "../../services/api";
+import { CustomError } from "../common/CustomError";
+
+import { CustomLoading } from "../common/CustomLoading";
+import withLoading from "../hoc/withLoading";
+import withError from "../hoc/withError";
+import { EditAnswer } from "./EditAnswer";
+
+const EditAnswerWithErrorAndLoading = withLoading(
+  withError(EditAnswer, CustomError),
+  CustomLoading
+);
 
 const EditAnswerPage = () => {
   const { answerId } = useParams<{ answerId: string }>();
-  //   const { data: answer, isLoading } = useGetAnswerQuery(id);
+  const { data: answer, isLoading, error } = useGetAnswerByIdQuery(answerId);
 
   return (
     <div>
       <h1>Edit Answer</h1>
-      <div>{`editing answer ${answerId}`}</div>
+      <EditAnswerWithErrorAndLoading
+        answer={answer}
+        isLoading={isLoading}
+        error={error}
+      />
     </div>
   );
 };
