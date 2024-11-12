b27354ca8a449884392d000f78160d8e99d7ec35
Anel Danza <aneldanza23@gmail.com>
2024-11-12 16:13:35 -0500

    display username and created date for each list item

--- src/components/questions/QuestionsList.tsx
+++ src/components/questions/QuestionsList.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import DOMPurify from "dompurify";
 import { LimitedQuestionData } from "./types";
 import { QuestionTags } from "./QuestionTags";
+import moment from "moment";
 
 interface QuestionsListProps {
   questions: LimitedQuestionData[];
@@ -19,15 +20,15 @@ const QuestionsList: React.FC<QuestionsListProps> = ({ questions }) => {
 
   return (
     <div>
-      <ul className="flex flex-col divide-y">
+      <ul className="flex flex-col divide-y max-w-2xl">
         {questions.map((question) => (
           <li key={question.id} className="flex gap-4 py-3">
-            <div className="flex flex-col gap-4 text-xs sm:text-sm">
+            <div className="flex flex-col gap-4 text-xs sm:text-sm text-appGray-300">
               <div>{`${question.votes} votes`}</div>
               <div>{`${question.answers} answers`}</div>
             </div>
 
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 flex-grow">
               <Link
                 to={`/questions/${question.id}`}
                 className="text-blue-500 text-sm"
@@ -46,6 +47,21 @@ const QuestionsList: React.FC<QuestionsListProps> = ({ questions }) => {
               />
 
               <QuestionTags tags={question.tags} />
+
+              <div className="self-end text-xs">
+                <span className="hyperlink">
+                  {question.user.id ? (
+                    <Link to={`/users/${question.user.id}`}>
+                      {question.user.username}
+                    </Link>
+                  ) : (
+                    "deleted user"
+                  )}
+                </span>{" "}
+                <span className="text-gray-500">
+                  {moment(question.created_at).fromNow()}
+                </span>
+              </div>
             </div>
           </li>
         ))}
