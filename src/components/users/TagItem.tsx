4940477c8354092a0697f7109ab2102e176e3273
Anel Danza <aneldanza23@gmail.com>
2024-12-05 16:34:40 -0500

    create TagItem component

---
+++ src/components/users/TagItem.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import { Tag } from "../tags/Tag";
+import { formatCountString } from "../../services/utils";
+import { TagData } from "../tags/types";
+
+interface TagItemProps {
+  tag: TagData;
+  userId: number;
+  postsCount: number;
+}
+
+export const TagItem: FC<TagItemProps> = ({ tag, userId, postsCount }) => {
+  return (
+    <div className="activity-card-row items-center" key={tag.id}>
+      <Tag
+        key={tag.id}
+        tag={tag}
+        url={`/users/${userId}/search?tag=${tag.name}`}
+      />
+      <div className="text-xs">
+        {formatCountString(postsCount, "post", "posts")}
+      </div>
+    </div>
+  );
+};
