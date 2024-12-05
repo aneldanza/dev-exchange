7d8969383e291439c089755679e14900efc4b838
Anel Danza <aneldanza23@gmail.com>
2024-12-05 15:07:20 -0500

    load UserProfile with Error and Loading components

--- src/components/users/UserPage.tsx
+++ src/components/users/UserPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useParams } from "react-router-dom";
 import { useShowFullUserInfoQuery } from "../../services/api";
 import { withLoading } from "../hoc/withLoading";
@@ -6,30 +6,58 @@ import { withError } from "../hoc/withError";
 import { CustomError } from "../common/CustomError";
 import { CustomLoading } from "../common/CustomLoading";
 import { UserProfile } from "./UserProfile";
-import { FullUserData } from "./types";
+import { UserContext } from "./UserContext";
+import { QuestionData } from "../questions/types";
+import { UserAnswerData } from "./types";
+import { TagData } from "../tags/types";
 
-interface UserPageProps {}
-
-// TODO: re-fecth data when current user changes
-
-// First, wrap MyComponent with withError, then pass the resulting component to withLoading
+// First, wrap UserProfile with withError, then pass the resulting component to withLoading
 const UserProfileWithErrorAndLoading = withLoading(
-  withError<{ data: FullUserData }>(UserProfile, CustomError),
+  withError(UserProfile, CustomError),
   CustomLoading
 );
 
-export const UserPage: React.FC<UserPageProps> = () => {
+export const UserPage: React.FC = () => {
   const { userId } = useParams<{ userId: string }>();
-  const { data, error, isLoading } = useShowFullUserInfoQuery(userId, {
+  const { data, error, isLoading } = useShowFullUserInfoQuery(userId || "", {
     refetchOnMountOrArgChange: true,
   });
 
+  const aggregateTags = useCallback(
+    (questions: QuestionData[], answers: UserAnswerData[]) => {
+      const tags: Record<
+        string,
+        { tag: TagData; posts: (QuestionData | UserAnswerData)[] }
+      > = {};
+
+      const posts = [...questions, ...answers];
+
+      posts.forEach((post) => {
+        post.tags.forEach((tag) => {
+          if (tags[tag.id]) {
+            (tags[tag.id].posts as (QuestionData | UserAnswerData)[]).push(
+              post
+            );
+          } else {
+            tags[tag.id] = {
+              tag,
+              posts: [post],
+            };
+          }
+        });
+      });
+
+      return tags;
+    },
+    []
+  );
+
+  const postsByTag = aggregateTags(data?.questions || [], data?.answers || []);
+
   return (
-    <UserProfileWithErrorAndLoading
-      data={data}
-      error={error}
-      isLoading={isLoading}
-    />
+    <UserContext.Provider value={{ fullUserData: data, postsByTag }}>
+      <UserProfileWithErrorAndLoading error={error} isLoading={isLoading} />
+    </UserContext.Provider>
   );
 };
 
