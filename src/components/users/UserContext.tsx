04101b0a51914d200a0bed08b287d44169e4bbd6
Anel Danza <aneldanza23@gmail.com>
2024-12-05 16:38:18 -0500

    export PostsByTag type

--- src/components/users/UserContext.tsx
+++ src/components/users/UserContext.tsx
@@ -3,12 +3,14 @@ import { FullUserData, UserAnswerData } from "./types";
 import { TagData } from "../tags/types";
 import { QuestionData } from "../questions/types";
 
+export type PostsByTag = {
+  tag: TagData;
+  posts: (QuestionData | UserAnswerData)[];
+};
+
 interface UserContextProps {
   fullUserData: FullUserData | undefined;
-  postsByTag: Record<
-    string,
-    { tag: TagData; posts: (QuestionData | UserAnswerData)[] }
-  >;
+  postsByTag: Record<string, PostsByTag>;
   //   setFullUserData: React.Dispatch<React.SetStateAction<FullUserData | null>>;
 }
 
