85e65901b1ecaa8994092c1f0d221ddf2efe47ca
Anel Danza <aneldanza23@gmail.com>
2024-12-05 13:16:15 -0500

    create reusable component to display question or answer in a post list

---
+++ src/components/common/PostListItem.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import DOMPurify from "dompurify";
+import moment from "moment";
+import { TbMessageQuestion, TbMessageExclamation } from "react-icons/tb";
+import { QuestionTags } from "../questions/QuestionTags";
+import { Post } from "../users/types";
+import { formatCountString } from "../../services/utils";
+
+interface PostListItemProps {
+  item: { post: Post; type: "question" | "answer" };
+}
+
+export const PostListItem: React.FC<PostListItemProps> = ({ item }) => {
+  const { post, type } = item;
+  return (
+    <li className="flex flex-col sm:flex-row gap-4 py-3">
+      <div className="flex flex-row sm:flex-col gap-4 text-xs sm:text-sm text-appGray-300">
+        <div>{formatCountString(post.votes, "vote", "votes")}</div>
+        {post.answers && (
+          <div>{formatCountString(post.answers, "answer", "answers")}</div>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-2 flex-grow">
+        <div className="flex gap-4 items-center">
+          <div>
+            {type === "question" ? (
+              <TbMessageQuestion size={22} color="blue-500" />
+            ) : (
+              <TbMessageExclamation
+                style={{ transform: "scaleX(-1)" }}
+                color="green"
+                size={22}
+              />
+            )}
+          </div>
+          <Link to={`/questions/$post.id}`} className="text-blue-500 text-sm">
+            {post.title}
+          </Link>
+        </div>
+
+        <div
+          className="text-xs"
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(
+              post.body.length > 100
+                ? `${post.body.substring(0, 100)}...`
+                : post.body
+            ),
+          }}
+        />
+
+        <QuestionTags tags={post.tags} />
+
+        <div className="self-end text-xs">
+          <span className="hyperlink">
+            {post.user.id ? (
+              <Link to={`/users/${post.user.id}`}>{post.user.username}</Link>
+            ) : (
+              "deleted user"
+            )}
+          </span>{" "}
+          <span className="text-gray-500">
+            {moment(post.created_at).fromNow()}
+          </span>
+        </div>
+      </div>
+    </li>
+  );
+};
