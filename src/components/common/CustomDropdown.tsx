214bb665f16046e8f8484c2a1bb41c1c82085b72
Anel Danza <aneldanza23@gmail.com>
2024-12-05 21:38:06 -0500

    create dropdown component with flowbite-react

---
+++ src/components/common/CustomDropdown.tsx
@@ -0,0 +1,29 @@
+import { Dropdown } from "flowbite-react";
+
+interface CustomDropdownProps {
+  options: string[];
+  handleOptionSelect: (option: string) => void;
+  selectedOption: string;
+}
+
+const dropdownTheme = {
+  content: "bg-gray-100",
+  inlineWrapper:
+    "border border-appGray-400 flex items-center justify-between px-2 py-1 rounded cursor-pointer min-w-32 w-full",
+};
+
+export const CustomDropdown: React.FC<CustomDropdownProps> = ({
+  options,
+  handleOptionSelect,
+  selectedOption,
+}) => {
+  return (
+    <Dropdown label={selectedOption} inline color="info" theme={dropdownTheme}>
+      {options.map((option) => (
+        <Dropdown.Item key={option} onClick={() => handleOptionSelect(option)}>
+          {option}
+        </Dropdown.Item>
+      ))}
+    </Dropdown>
+  );
+};
