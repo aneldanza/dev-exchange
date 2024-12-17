22edac301ee9595fb7990d19e5734ada3c625301
Anel Danza <aneldanza23@gmail.com>
2024-12-17 16:18:44 -0500

    add more attributes to PostsSearchResponse type

--- src/components/search/types.ts
+++ src/components/search/types.ts
@@ -4,6 +4,7 @@ export type PostsSearchPayload = {
   page: number;
   value: string;
   sort?: string;
+  limit?: number;
 };
 
 export type PostsSearchResponse = {
@@ -11,4 +12,8 @@ export type PostsSearchResponse = {
   total_pages: number;
   current_page: number;
   total_results: number;
+  next_page: number | null;
+  prev_page: number | null;
+  first_page: boolean;
+  last_page: boolean;
 };
