9f86e2e61ee1bba285d1e0cdce462afb91504a20
Anel Danza <aneldanza23@gmail.com>
2024-12-17 17:57:45 -0500

    handle search icon display for mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,8 @@ export default function App() {
   return (
     <>
       <div className="font-sans text-appBlack w-full h-screen overflow-hidden">
-        <nav className="px-4 border-b-appGray-100 border sticky">
-          <Header />
-        </nav>
+        <Header />
+
         <main className=" flex h-full container mx-auto">
           <div className="hidden min-w-40 max-w-64 grow-1 border py-6 border-appGray-100 border-t-0 border-l-0 sm:block h-screen">
             <Menu />
--- src/components/header/TopNavigationBar.tsx
+++ src/components/header/TopNavigationBar.tsx
@@ -1,6 +1,10 @@
 import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  Bars3Icon,
+  XMarkIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
 import { useSignOutMutation } from "../../services/api";
 import { useAuth } from "../../services/storeHooks";
 import { Menu } from "../layout/Menu";
@@ -12,6 +16,7 @@ export const TopNavigationBar = () => {
   const { user, setUser } = useAuth();
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
   const sideBarRef = useRef<HTMLDivElement>(null);
+  const [showSearchBar, setShowSearchBar] = useState(false);
 
   const handleLogOut = async () => {
     try {
@@ -49,61 +54,79 @@ export const TopNavigationBar = () => {
   }, []);
 
   return (
-    <div className="relative container mx-auto">
-      <div className="flex flex-row w-full items-center space-x-3 p-2 border-b-1 text-xs justify-between">
-        <div className="sm:hidden">
-          {isSideBarOpen ? (
-            <XMarkIcon
-              className="size-6 cursor-pointer"
-              onClick={toggleSideBar}
+    <nav className=" border-b-appGray-100 border-x-0 border-t-2 border-t-appOrange  border-b sticky">
+      <div className="relative mx-auto">
+        <div className="flex flex-row w-full items-center space-x-3 h-full border-b-1 text-xs justify-between py-2 pr-2">
+          <div className="sm:hidden h-full flex items-center justify-center shrink-0 px-2">
+            {isSideBarOpen ? (
+              <XMarkIcon
+                className="size-6 cursor-pointer"
+                onClick={toggleSideBar}
+              />
+            ) : (
+              <Bars3Icon
+                className="size-6 cursor-pointer"
+                onClick={toggleSideBar}
+              />
+            )}
+          </div>
+
+          <div className="text-blue ">Logo</div>
+
+          <div className={`sm:hidden h-full`}>
+            <MagnifyingGlassIcon
+              className={`size-6 cursor-pointer ${
+                showSearchBar && "text-blue-500"
+              }`}
+              onClick={() => setShowSearchBar(!showSearchBar)}
             />
-          ) : (
-            <Bars3Icon
-              className="size-6 cursor-pointer"
-              onClick={toggleSideBar}
-            />
-          )}
+          </div>
+          <div className="hidden sm:block w-2/3">
+            <SearchInput />
+          </div>
+          <nav className="flex shrink-0 space-x-2 items-center justify-between">
+            {user ? (
+              <>
+                <Link
+                  to={`/users/${user.id}`}
+                  className="font-semibold cursor-pointer"
+                >
+                  {user.username}
+                </Link>
+
+                <button className="btn btn-secondary" onClick={handleLogOut}>
+                  Log out
+                </button>
+              </>
+            ) : (
+              <>
+                <Link to={"/login"} className="btn btn-secondary">
+                  Log in
+                </Link>
+                <Link to={"/signup"} className="btn btn-primary">
+                  Sign up
+                </Link>
+              </>
+            )}
+          </nav>
         </div>
 
-        <div className="text-blue ">Logo</div>
-        <div className="w-2/3">
-          <SearchInput />
-        </div>
-        <nav className="flex shrink-0 space-x-2 items-center justify-between">
-          {user ? (
-            <>
-              <Link
-                to={`/users/${user.id}`}
-                className="font-semibold cursor-pointer"
-              >
-                {user.username}
-              </Link>
-
-              <button className="btn btn-secondary" onClick={handleLogOut}>
-                Log out
-              </button>
-            </>
-          ) : (
-            <>
-              <Link to={"/login"} className="btn btn-secondary">
-                Log in
-              </Link>
-              <Link to={"/signup"} className="btn btn-primary">
-                Sign up
-              </Link>
-            </>
-          )}
-        </nav>
+        {isSideBarOpen && (
+          <div
+            ref={sideBarRef}
+            className="absolute bg-white py-6 left-0 shadow-md w-64"
+          >
+            <Menu handleOptionSelect={() => setIsSideBarOpen(false)} />
+          </div>
+        )}
       </div>
-
-      {isSideBarOpen && (
-        <div
-          ref={sideBarRef}
-          className="absolute bg-white py-6 left-0 shadow-md w-64"
-        >
-          <Menu handleOptionSelect={() => setIsSideBarOpen(false)} />
+      {showSearchBar && (
+        <div className="sm:hidden absolute w-full left-0 bg-appGray-50 py-2 px-2 flex">
+          <div className="w-full">
+            <SearchInput />
+          </div>
         </div>
       )}
-    </div>
+    </nav>
   );
 };
