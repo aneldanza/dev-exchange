22ba34d5a4ed0cb06c4181204a78de2bf7b4c15f
Anel Danza <aneldanza23@gmail.com>
2025-01-01 14:26:04 -0500

    receive handleSearch function as a prop

--- src/components/header/SearchInput.tsx
+++ src/components/header/SearchInput.tsx
@@ -2,20 +2,12 @@ import React from "react";
 import { TextInput, TextInputProps } from "flowbite-react";
 import { Form, Formik, Field } from "formik";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { useNavigate } from "react-router-dom";
 
 interface SearchInputProps {
-  hideInput?: () => void;
+  handleSearch: (values: { search: string }) => void;
 }
 
-const SearchInput: React.FC<SearchInputProps> = ({ hideInput }) => {
-  const navigate = useNavigate();
-
-  const handleSearch = (values: { search: string }) => {
-    navigate(`/search?q=${values.search}`);
-    hideInput && hideInput();
-  };
-
+const SearchInput: React.FC<SearchInputProps> = ({ handleSearch }) => {
   const initialValues = {
     search: "",
   };
@@ -36,7 +28,7 @@ const SearchInput: React.FC<SearchInputProps> = ({ hideInput }) => {
                   theme={{
                     field: {
                       input: {
-                        base: "flex flex-grow-1 w-full text-lg",
+                        base: "flex flex-grow-1 w-full text-sm",
                         colors: {
                           gray: "border-appGray-100 focus:border-blue-700 focus:ring-blue-700",
                         },
