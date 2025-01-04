d0bc5bfd36ef4ca0cd7570ff542f4e42c49acb67
Anel Danza <aneldanza23@gmail.com>
2025-01-04 17:01:39 -0500

    make Header fixed at the top of the screen

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ export default function App() {
       <div className="flex flex-col min-h-screen font-sans text-appBlack">
         <Header />
 
-        <main className="flex flex-grow app-container">
-          <div className="hidden min-w-40 max-w-64 flex-grow border py-6 border-appGray-100 border-y-0 border-l-0 sm:block">
+        <main className="flex flex-grow mt-16">
+          <div className="hidden sm:block min-w-40 max-w-64 flex-grow border border-appGray-100 border-y-0 border-l-0 py-6">
             <Menu />
           </div>
           <div className="px-4 py-6 w-full h-full overflow-auto">
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import { TopNavigationBar } from "../header/TopNavigationBar";
 
 export const Header = () => {
   return (
-    <div className="z-50 border-b-appGray-100 border-x-0 border-t-2 border-t-appOrange border-b sticky">
+    <div className="fixed top-0 left-0 w-full z-50 bg-white border-b-appGray-100 border-x-0 border-t-2 border-t-appOrange border-b">
       <TopNavigationBar />
     </div>
   );
