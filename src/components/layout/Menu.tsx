ad0dd890ab7659b25141880a97cba11aa7744d8a
Anel Danza <aneldanza23@gmail.com>
2025-01-06 14:34:35 -0500

    highlight current menu section base on the current page

--- src/components/layout/Menu.tsx
+++ src/components/layout/Menu.tsx
@@ -1,4 +1,5 @@
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import { HomeIcon, UsersIcon, TagIcon } from "@heroicons/react/20/solid";
 import Button from "../common/Button";
 
@@ -8,6 +9,26 @@ interface MenuProps {
 
 export const Menu: React.FC<MenuProps> = ({ handleOptionSelect }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [currentMenuSection, setCurrentMenuSection] = useState("home");
+
+  useEffect(() => {
+    // remove trailing slash
+    const path = window.location.pathname.replace(/\/$/, "");
+    switch (path) {
+      case "/":
+        setCurrentMenuSection("home");
+        break;
+      case "/users":
+        setCurrentMenuSection("users");
+        break;
+      case "/tags":
+        setCurrentMenuSection("tags");
+        break;
+      default:
+        setCurrentMenuSection("home");
+    }
+  }, [location.pathname]);
 
   const handleClick = (url: string) => {
     navigate(url);
@@ -15,22 +36,30 @@ export const Menu: React.FC<MenuProps> = ({ handleOptionSelect }) => {
   };
 
   return (
-    <ul className="menu list space-y-2 text-sm">
-      <li className="menu-item">
+    <ul className="menu list space-y-2 text-sm shadow-sm">
+      <li
+        className={`menu-item ${currentMenuSection === "home" ? "active" : ""}`}
+      >
         <Button
           title="Home"
           onClick={handleClick.bind(null, "/")}
           icon={<HomeIcon className="w-4" />}
         />
       </li>
-      <li className="menu-item">
+      <li
+        className={`menu-item ${
+          currentMenuSection === "users" ? "active" : ""
+        }`}
+      >
         <Button
           title="Users"
           onClick={handleClick.bind(null, "/users")}
           icon={<UsersIcon className="w-4" />}
         />
       </li>
-      <li className="menu-item">
+      <li
+        className={`menu-item ${currentMenuSection === "tags" ? "active" : ""}`}
+      >
         <Button
           title="Tags"
           onClick={handleClick.bind(null, "/tags")}
