d35512028159616f5e1bf1815f5ebe96858be49a
Anel Danza <aneldanza23@gmail.com>
2025-03-11 15:43:26 -0400

    accept optional text size

--- src/components/common/MarkDownViewer.tsx
+++ src/components/common/MarkDownViewer.tsx
@@ -3,14 +3,20 @@ import MDEditor from "@uiw/react-md-editor";
 
 interface MarkdownViewerProps {
   content: string;
+  textSize?: string;
 }
 
-const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ content }) => {
+const MarkdownViewer: React.FC<MarkdownViewerProps> = ({
+  content,
+  textSize,
+}) => {
   return (
     <div className="w-full text-appBlack" data-color-mode="github-light">
       <MDEditor.Markdown
         source={content}
-        className="break-words whitespace-pre-wrap prose max-w-full"
+        className={`break-words whitespace-pre-wrap prose max-w-full ${
+          textSize ? textSize : "prose-sm"
+        }`}
       />
     </div>
   );
