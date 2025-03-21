1ef2530f3a2bcd740fc8db6b2d9dca858261f05b
Anel Danza <aneldanza23@gmail.com>
2025-03-21 12:59:44 -0400

    Add ListSubheader and sortTabs skeletons

---
+++ src/components/common/ListSubheaderSkeleton.tsx
@@ -0,0 +1,18 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import SortTabsSkeleton from "./SortTabsSkeleton";
+
+const ListSubheaderSkeleton = () => {
+  return (
+    <div className="mb-6 pb-6 border-b subheader-wrap">
+      <div className="font-medium text-sm flex shrink-0">
+        <Skeleton width={100} />
+      </div>
+      <div className="flex text-xs shrink-0">
+        <SortTabsSkeleton />
+      </div>
+    </div>
+  );
+};
+
+export default ListSubheaderSkeleton;
---
+++ src/components/common/SortTabsSkeleton.tsx
@@ -0,0 +1,16 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const SortTabsSkeleton = () => {
+  return (
+    <div className="flex gap-2">
+      {[...Array(3)].map((_, index) => (
+        <div key={index} className="flex items-center gap-2">
+          <Skeleton width={50} height={20} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SortTabsSkeleton;
